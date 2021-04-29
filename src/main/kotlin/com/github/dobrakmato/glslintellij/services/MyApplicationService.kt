3aa7b89a6f9af9338b2bd4e513e58739f7fafd4a
GitHub Action <action@github.com>
2021-04-29 12:24:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/dobrakmato/glslintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dobrakmato/glslintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dobrakmato.glslintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dobrakmato.glslintellij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/dobrakmato/glslintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dobrakmato/glslintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dobrakmato.glslintellij.services
 
+import com.github.dobrakmato.glslintellij.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
